7726a301954d48bfdf03f685b417c6d83de89a53
Brandon Walden <brandonshanewalden@gmail.com>
2017-08-29 13:59:52 -0400

    working on imageUrl

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import '../styles/App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
 
@@ -26,16 +26,25 @@ class App extends Component {
   }
 }
 
-componentWillMount() {
-  const API_KEY = 'https://api.nasa.gov/planetary/apod?api_key=HZKOoyDcTtZBGN3Wwg3uR7DDKbKiM8351Sg6X6of'
+fetchRoverImage() {
+  this.setState({camera: this.state.camera, rover: this.state.rover, sol: this.state.sol});
+    let cam = this.state.camera;
+    let rove = this.state.rover;
+    let num = this.state.sol;
 
-  fetch(API_KEY).then((response) => {
-    return response.json();
-  })
-  .then((data) => {
+    let imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rove}/photos?sol=${num}&camera=${cam}&api_key=${API_KEY}`;
 
+    const API_KEY = 'https://api.nasa.gov/planetary/apod?api_key=HZKOoyDcTtZBGN3Wwg3uR7DDKbKiM8351Sg6X6of'
+
+    fetch(API_KEY).then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      this.state = {
+      rover: "Curiosity",
+      camera: "FHAZ",
+      images: [],
+      sol: "",
+    }
   });
 }
-
-
-export default App;
