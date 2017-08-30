d801f92e8879aa9e4ecaef75595bfa23e24baa27
Brandon Walden <brandonshanewalden@gmail.com>
2017-08-30 10:51:17 -0400

    moved fetchRover into GetImageForm and setup camera views url

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,15 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
+import GetImageForm from './GetImageForm'
 
 export default class App extends Component {
   render() {
     return (
-
       <div>
-        <label htmlFor="rover">Rover</label>
-        <select onChange={this.handleRover} id="rover" value={this.state.value}>
-          <option value="Curiosity">Curiosity</option>
-          <option value="Opportunity">Opportunity</option>
-          <option value="Spirit">Spirt</option>
-        </select>
-        <label htmlFor="camera">Camera Type</label>
-        <select onChange={this.handleCamera} id="rover" value={this.state.value}>
-          <option value="fhaz">FHAZ (Front Hazard)</option>
-          <option value="rhaz">RHAZ (Rear Hazard)</option>
-          <option value="navcam">NAVCAM (Navigation Cam)</option>
-        </select>
-        <label htmlFor="sol">Martian Sol: 1000-2000</label>
-        <input type="number" onChange={this.handleSol} max="2000" min="1000" value={this.state.value}/>
+        <logo />
+        <GetImageForm />
       </div>
     );
   }
-}
-
-function fetchRoverImage() {
-  this.setState({camera: this.state.camera, rover: this.state.rover, sol: this.state.sol});
-    let cam = this.state.camera;
-    let rove = this.state.rover;
-    let num = this.state.sol;
-
-    let imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rove}/photos?sol=${num}&camera=${cam}&api_key=${API_KEY}`;
-
-    const API_KEY = 'https://api.nasa.gov/planetary/apod?api_key=HZKOoyDcTtZBGN3Wwg3uR7DDKbKiM8351Sg6X6of'
-
-    fetch(API_KEY).then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      this.state = {
-      rover: "Curiosity",
-      camera: "FHAZ",
-      images: [],
-      sol: "",
-    }
-  });
 }
--- src/components/GetImageButton.js
+++ src/components/GetImageButton.js
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+
+export default class GetImageButton extends Component{
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.handleClick}>Change View</button>
+      </div>
+    )
+  }
+
+}
